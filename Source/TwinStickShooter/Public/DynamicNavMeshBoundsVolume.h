844f33135cd7961f3f762390a1558128297a1e8b
ctmurphy77 <rajonwitness@gmail.com>
2018-08-19 23:11:05 -0400

    added Matt's AI blueprints

---
+++ Source/TwinStickShooter/Public/DynamicNavMeshBoundsVolume.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NavMesh/NavMeshBoundsVolume.h"
+#include "DynamicNavMeshBoundsVolume.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TWINSTICKSHOOTER_API ADynamicNavMeshBoundsVolume : public ANavMeshBoundsVolume
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
